19b9619fa5146219ee31fc1373dde95cf65883c3
Fernando Quisaguano <ferjeff92@gmail.com>
2024-06-10 18:22:00 -0500

    Creacion de frontend

--- backend/app/server/app.py
+++ backend/app/server/app.py
@@ -1,9 +1,23 @@
-from fastapi import FastAPI
+import os
 from app.server.routes.task import router as UserRouter
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+load_dotenv()
 
 app = FastAPI()
 
-app.include_router(UserRouter, tags=["Task"], prefix="/task")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[os.getenv("FRONTEND_URL")],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(UserRouter, tags=["Task"], prefix="/api/tasks")
 
 
 @app.get("/", tags=["Root"])
